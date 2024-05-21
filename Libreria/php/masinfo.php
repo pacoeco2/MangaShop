9095d9ed64e211422db430cc3f038bbde14ca6f5
Jorge Francisco <jacosta23@alumnos.uaq.mx>
2024-05-21 12:18:35 -0600

    commit

--- Libreria/php/masinfo.php
+++ Libreria/php/masinfo.php
@@ -46,6 +46,7 @@ if($query){
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" type = "text/css" href="../css/styles_MI.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
